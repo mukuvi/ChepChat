c308bac01b6336f5c05f22eb439c4f77141f065c
mukuvi <jamesmngandu@gmail.com>
2025-03-28 17:53:48 +0300

    chepchat

--- chepchat/app/index.tsx
+++ chepchat/app/index.tsx
@@ -1,7 +1,7 @@
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
-export default function Index() {
+export default function Home() {
   return (
     <View
       style={{
